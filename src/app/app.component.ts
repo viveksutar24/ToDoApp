c7d65c7cffc1fc1d49c94ad2f8ecfc987ab4111f
Vivek Sutar <viveksutar26@gmail.com>
2023-05-01 16:24:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  tasks: any = []
+
+  ngOnInit(): void {
+
+  }
+
   title = 'mytodoapp';
+
+  newTask(content: string) {
+    // console.log(content);
+    if (content != '') {
+      this.tasks.push({ id: this.tasks.length, title: content })
+      console.log(this.tasks);
+    }
+  }
+
+  removeTask(id: any) {
+    this.tasks = this.tasks.filter((item: { id: any }) => item.id != id)
+  }
+
+  removeAll() {
+    this.tasks = []
+  }
 }
